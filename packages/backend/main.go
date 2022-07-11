2cea549a490b405b21277acb08dba8fcdd49d1f2
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-11 17:27:12 +0800

    🎨 Switch to go and gin

---
+++ packages/backend/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.SetTrustedProxies(nil)
+	r.GET("/", func(c *gin.Context) {
+		data := gin.H{"hello": "Yu Hanwen"}
+		c.JSON(http.StatusOK, data)
+	})
+	r.Run("localhost:3001")
+}
