20b923d985bb8e72c03584eda9bbb622813c4667
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-07-13 19:37:27 +0800

    🎨

--- packages/backend/handlers/GetHandler.go
+++ packages/backend/handlers/GetHandler.go
@@ -3,13 +3,15 @@ package handlers
 import (
 	"net/http"
 
-	"taskism/structs"
-
 	"github.com/gin-gonic/gin"
 )
 
+type PersonGet struct {
+	ID string `uri:"id" binding:"required,uuid"`
+}
+
 func GetHandler(c *gin.Context) {
-	var person structs.Person
+	var person PersonGet
 	if err := c.ShouldBindUri(&person); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
